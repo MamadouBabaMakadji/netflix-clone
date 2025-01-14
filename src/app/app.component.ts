526a3a5eaed6436e2b390294c11a09242b5ab72c
MamadouBabaMakadji <mamadoubaba1@hotmail.fr>
2025-01-14 16:55:33 +0100

    Add bootstrap and fontawesome

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { appFontawesomeIcons } from './shared/fontawesome-icons';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +9,14 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'netflix-clone';
+  faIconLibrary = inject(FaIconLibrary);
+
+  ngOnInit(): void {
+    this.initFontawesomeIcons();
+  }
+  initFontawesomeIcons(): void {
+    this.faIconLibrary.addIcons(...appFontawesomeIcons);
+  }
 }
---
+++ src/app/shared/fontawesome-icons.ts
@@ -0,0 +1,6 @@
+import { IconDefinition } from '@fortawesome/free-brands-svg-icons';
+import { faPlay } from '@fortawesome/free-solid-svg-icons';
+
+export const appFontawesomeIcons: IconDefinition[] = [
+    faPlay
+];
